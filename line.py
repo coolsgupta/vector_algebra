5483ad47d8027eb1a8da61330bb3b04cc60bbe43
= <=>
2017-01-27 21:31:48 +0530

    pushed code on branch

---
+++ line.py
@@ -0,0 +1,179 @@
+from decimal import getcontext,Decimal
+from Vectors import Vector
+
+getcontext().prec = 30
+
+
+class Line(object):
+
+    NO_NONZERO_ELTS_FOUND_MSG = 'No nonzero elements found'
+
+    def __init__(self, normal_vector=None, constant_term=None):
+        self.dimension = 2
+
+        if not normal_vector:
+            all_zeros = ['0']*self.dimension
+            normal_vector = Vector(all_zeros)
+        self.normal_vector = normal_vector
+
+        if not constant_term:
+            constant_term = Decimal('0')
+        self.constant_term = Decimal(constant_term)
+
+        self.set_basepoint()
+
+
+    def set_basepoint(self):
+        try:
+            n = self.normal_vector
+            c = self.constant_term
+            basepoint_coords = ['0']*self.dimension
+
+            initial_index = Line.first_nonzero_index(n)
+            initial_coefficient = n[initial_index]
+
+            basepoint_coords[initial_index] = c/initial_coefficient
+            self.basepoint = Vector(basepoint_coords)
+
+        except Exception as e:
+            if str(e) == Line.NO_NONZERO_ELTS_FOUND_MSG:
+                self.basepoint = None
+            else:
+                raise e
+
+
+    def __str__(self):
+
+        num_decimal_places = 3
+
+        def write_coefficient(coefficient, is_initial_term=False):
+            coefficient = round(coefficient, num_decimal_places)
+            if coefficient % 1 == 0:
+                coefficient = int(coefficient)
+
+            output = ''
+
+            if coefficient < 0:
+                output += '-'
+            if coefficient > 0 and not is_initial_term:
+                output += '+'
+
+            if not is_initial_term:
+                output += ' '
+
+            if abs(coefficient) != 1:
+                output += '{}'.format(abs(coefficient))
+
+            return output
+
+        n = self.normal_vector
+
+        try:
+            initial_index = Line.first_nonzero_index(n)
+            terms = [write_coefficient(n[i], is_initial_term=(i==initial_index)) + 'x_{}'.format(i+1)
+                     for i in range(self.dimension) if round(n[i], num_decimal_places) != 0]
+            output = ' '.join(terms)
+
+        except Exception as e:
+            if str(e) == self.NO_NONZERO_ELTS_FOUND_MSG:
+                output = '0'
+            else:
+                raise e
+
+        constant = round(self.constant_term, num_decimal_places)
+        if constant % 1 == 0:
+            constant = int(constant)
+        output += ' = {}'.format(constant)
+
+        return output
+
+
+    @staticmethod
+    def first_nonzero_index(iterable):
+        for k, item in enumerate(iterable):
+            if not MyDecimal(item).is_near_zero():
+                return k
+        raise Exception(Line.NO_NONZERO_ELTS_FOUND_MSG)
+
+
+    def if_parallel_lines(self,l2):
+        if self.normal_vector.parallel_check(l2.normal_vector):
+            return True
+        else:
+            return False
+
+
+    def if_equal_lines(self,l2):
+        x = self.basepoint[0] - l2.basepoint[0]
+        y = self.basepoint[1] - l2.basepoint[1]
+
+        if self.normal_vector.orthogonal_check(Vector([x,y])):
+            return True
+        else:
+            return False
+
+
+    def swap(self,l2):
+        t = self.normal_vector[0]
+        self.normal_vector[0] = l2.normal_vector[0]
+        l2.normal_vector[0] = t
+
+        t = self.normal_vector[1]
+        self.normal_vector[1] = l2.normal_vector[1]
+        l2.normal_vector[1] = t
+
+        t = self.basepoint[0]
+        self.basepoint[0] = l2.basepoint[0]
+        l2.basepoint[0] = t
+
+        t = self.basepoint[1]
+        self.basepoint[1] = l2.basepoint[1]
+        l2.basepoint[1] = t
+
+        t = self.constant_term
+        self.constant_term = l2.constant_term
+        l2.constant_term = t
+
+
+
+    def find_intersection(self,l2):
+
+        if self.if_equal_lines(l2):
+            print "coincident lines"
+
+        elif self.if_parallel_lines(l2):
+            print "lines are parallel"
+
+        else:
+            if (self.normal_vector[0]==0):
+                self.swap(l2)
+
+            a = self.normal_vector[0]
+            b = self.normal_vector[1]
+            k1 = self.constant_term
+            c = l2.normal_vector[0]
+            d = l2.normal_vector[1]
+            k2 = l2.constant_term
+
+            x = Decimal(((d*k1) - (b*k2))/((a*d) - (b*c)))
+            y = Decimal(((a*k2) - (c*k1))/((a*d) - (b*c)))
+
+            print "point of intersection : (%d,%d)"%(x,y)
+
+
+class MyDecimal(Decimal):
+    def is_near_zero(self, eps=1e-10):
+        return abs(self) < eps
+
+
+l1 = Line(normal_vector=Vector[4.046,2.836],constant_term=1.21)
+l2 = Line(normal_vector=Vector[10.115,7.09],constant_term=3.025)
+l1.find_intersection(l2)
+
+l1 = Line(normal_vector=Vector[7.204,3.182],constant_term=8.68)
+l2 = Line(normal_vector=Vector[8.172,4.114],constant_term=9.883)
+l1.find_intersection(l2)
+
+l1 = Line(normal_vector=Vector[1.182,5.562],constant_term=6.744)
+l2 = Line(normal_vector=Vector[1.773,8.343],constant_term=9.525)
+l1.find_intersection(l2)
