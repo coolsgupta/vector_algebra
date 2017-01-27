e699081980c4a46afbf0eef35d493ae0303329c1
= <=>
2017-01-27 21:35:23 +0530

    pushed code on branch

--- line.py
+++ line.py
@@ -173,7 +173,3 @@ l1.find_intersection(l2)
 l1 = Line(normal_vector=Vector[7.204,3.182],constant_term=8.68)
 l2 = Line(normal_vector=Vector[8.172,4.114],constant_term=9.883)
 l1.find_intersection(l2)
-
-l1 = Line(normal_vector=Vector[1.182,5.562],constant_term=6.744)
-l2 = Line(normal_vector=Vector[1.773,8.343],constant_term=9.525)
-l1.find_intersection(l2)
